d4f56e96443181bdc760254854ce1b7e319d00eb
rapidlee <49045122+rapidlee@users.noreply.github.com>
2019-05-02 11:49:38 -0700

    Add files via upload

---
+++ main_app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from testapp import test_domain_input, find_pattern
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def my_form():
+    return render_template('my-form.html')
+
+# fetch input, then pass vars to html and process
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['some_input']
+    final_input = test_domain_input(text)
+    final_output = find_pattern(final_input)
+    return render_template('my-form-processed.html', final_output=final_output, final_input=final_input)
+
+if __name__ == "__main__":
+      app.run(debug=True)
+ 
---
+++ testapp.py
@@ -0,0 +1,34 @@
+import re
+
+# Read the OpenGrok file and save it to a var
+with open('open_grok.txt', 'r') as f:
+    print(f'The filename you are searching from is: {f.name}')
+    file_contents = f.read()
+    
+# Test input for @yelp domain, if not then add the @yelp domain to the input
+def test_domain_input(original_input):
+    clean_input = original_input.strip()
+    if re.search(r'\@yelp.com', clean_input):
+        return clean_input
+    else:
+        # bprint('There is no @ sign')
+        clean_input += '@yelp.com'
+        return clean_input  
+
+# Find the pattern in file_contents using the final_input var
+def find_pattern(final_input):
+    list_output = []
+    # If input pattern is found, split results in 2 and search 2nd group for pattern
+    if re.search(final_input, file_contents):
+        first_result = re.search(f'({final_input})(.+)', file_contents)
+        first_result_2nd_group = first_result.group(2)
+        # Now that we have the 2nd group we search for =cn=g patterns and iterate it through
+        matches = re.findall(r'g-.+?,', first_result_2nd_group)
+        # print(f'Below are restricted groups in AD for {final_input}:')
+        for match in matches:
+            list_output.append(match[:-1])
+        return list_output
+        #print(first_result.group(2))
+    else:
+        return False
+
